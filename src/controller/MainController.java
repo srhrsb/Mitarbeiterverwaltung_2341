ef6fc2d1564e0dba4bd613d9f4561186b9de515a
srhrsb <sebastian.boettcher2@srh.de>
2024-07-03 13:31:23 +0200

    Mitarbeiterverwaltung mit UI

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,20 +1,30 @@
 package controller;
 import dao.EmployeeDAO;
+import view.MainView;
+
+import java.awt.event.ActionEvent;
 
 public class MainController {
     /**
      * Objekt mit dem die Mitarbeiterdaten zukünftig verwaltet werden
      */
     private EmployeeDAO employeeDB;
+    private MainView view;
 
-    public MainController( EmployeeDAO employeeDB ) {
+    public MainController( EmployeeDAO employeeDB, MainView view) {
         this.employeeDB = employeeDB;
-        Test();
+        this.view = view;
+
+        view.addGetButtonHandler(this::getEmployee);
+        view.addSaveButtonHandler(this::saveEmployee);
+        view.addDeleteButtonHandler(this::deleteEmployee);
+        //Test();
     }
 
     public static void main(String[] args) {
         EmployeeDAO employeeDAO = new EmployeeDAO();
-        new MainController( employeeDAO );
+        MainView view = new MainView( 500, 200);
+        new MainController( employeeDAO, view );
     }
 
     private void Test(){
@@ -38,6 +48,30 @@ public class MainController {
         return    lastname.substring(0,2)
                 + firstname.substring(0,2)
                 + (int)(Math.random() * 999999 );
+    }
+
+    /**
+     * Mitarbeiter mit Hilfe der ID anfordern und anzeigen
+     * @param event
+     */
+    private void getEmployee(ActionEvent event){
+        System.out.println("Action Event: " + event.getActionCommand());
+    }
+
+    /**
+     * Aktuell eingegebene Mitarbeiterdaten speichern
+     * @param event
+     */
+    private void saveEmployee(ActionEvent event){
+        System.out.println("Action Event: " + event.getActionCommand());
+    }
+
+    /**
+     * Mitarbeiter mit entsprechender ID löschen
+     * @param event
+     */
+    private void deleteEmployee(ActionEvent event){
+        System.out.println("Action Event: " + event.getActionCommand());
     }
 
 }
--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,6 +1,97 @@
 package view;
 
-public class MainView {
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionListener;
 
+public class MainView extends JFrame {
+
+    private  JButton getEmployeeBtn, saveEmployeeBtn, deleteEmployeeBtn;
+    private JTextField employeeIdTf, lastnameTf, firstnameTf, jobTf;
+
+
+    public MainView( int width, int height ){
+
+        setSize( width, height );
+        setFont( new Font("Arial", Font.PLAIN, 20));
+        setTitle("Mitarbeiterverwatung");
+        setDefaultCloseOperation( DISPOSE_ON_CLOSE );
+        addUIComponents();
+        setVisible(true);
+    }
+
+
+    /**
+     * Hinzufuegen von UI-Elementen
+     */
+    private void addUIComponents( ){
+
+        JPanel topPanel = new JPanel();
+        JPanel centerPanel = new JPanel();
+        JPanel bottomPanel = new JPanel();
+
+        getEmployeeBtn = new JButton("Mitarbeiter anzeigen");
+        saveEmployeeBtn = new JButton("Mitarbeiter speichern");
+        deleteEmployeeBtn = new JButton("Mitarbeiter löschen");
+
+        employeeIdTf = new JTextField();
+        lastnameTf = new JTextField();
+        firstnameTf = new JTextField();
+        jobTf = new JTextField();
+
+        JLabel employeeIdLabel = new JLabel("Mitarbeiter ID:");
+        JLabel lastnameLabel = new JLabel("Nachname:");
+        JLabel firstnameLabel = new JLabel("Vorname:");
+        JLabel jobLabel = new JLabel("Tätigkeit:");
+
+        JLabel topLabel = new JLabel("Mitarbeiterdaten hinzufügen, ändern oder löschen");
+
+        //einfügen
+        topPanel.add(topLabel);
+
+        centerPanel.setLayout( new GridLayout( 4, 2));
+        centerPanel.setBorder( new EmptyBorder(5,5,5,5));
+        centerPanel.add(employeeIdLabel);
+        centerPanel.add(employeeIdTf);
+        centerPanel.add(firstnameLabel);
+        centerPanel.add(firstnameTf);
+        centerPanel.add(lastnameLabel);
+        centerPanel.add(lastnameTf);
+        centerPanel.add(jobLabel);
+        centerPanel.add(jobTf);
+
+        bottomPanel.add(getEmployeeBtn);
+        bottomPanel.add(saveEmployeeBtn);
+        bottomPanel.add(deleteEmployeeBtn);
+
+        add(topPanel, BorderLayout.NORTH);
+        add(centerPanel, BorderLayout.CENTER);
+        add(bottomPanel, BorderLayout.SOUTH);
+    }
+
+    /**
+     * Eventlistener für GetEmpolyee Button hinzufügen
+     * @param listener
+     */
+    public void addGetButtonHandler(ActionListener listener){
+        getEmployeeBtn.addActionListener(listener);
+    }
+
+    /**
+     * Eventlistener für SaveEmpolyee Button hinzufügen
+     * @param listener
+     */
+    public void addSaveButtonHandler(ActionListener listener){
+        saveEmployeeBtn.addActionListener(listener);
+    }
+
+    /**
+     * Eventlistener für Delete Empolyee Button hinzufügen
+     * @param listener
+     */
+    public void addDeleteButtonHandler(ActionListener listener){
+        deleteEmployeeBtn.addActionListener(listener);
+    }
 
 }
